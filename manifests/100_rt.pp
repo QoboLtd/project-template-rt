b99c0e80a27b6cccf68c878ac80c7f4ad1de35e4
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2015-07-17 10:11:58 +0300

    MySQL moved to db.pp

--- manifests/100_rt.pp
+++ manifests/100_rt.pp
@@ -134,25 +134,6 @@ node default {
 		require => [ Package['nginx'], Package['spawn-fcgi'], File["/home/${::rt_user}"] ],
 	}
 
-	# We always need MySQL client
-	#class { '::mysql::client': }
-	include '::mysql::client'
-
-	# We always need bindings (dev for CPAN, perl otherwise)
-	class { '::mysql::bindings':
-		perl_enable => true,
-	}
-
-	case $::rt_setup_local_db {
-		'yes', '1', 'true': {
-			#class { '::mysql::server': }
-			include '::mysql::server'
-		}
-		default: {
-			notice("Skipping local DB setup due to .env settings")
-		}
-	}
-
 	# Get RT from GitHub
 	vcsrepo { $::rt_local_repo:
 		ensure => present,
